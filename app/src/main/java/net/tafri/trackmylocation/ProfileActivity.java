5dc847ea060972cf7c35139693744fc863cdda32
salman48 <rizvisalman48@gmail.com>
2020-12-28 14:49:23 +0530

    message

--- app/src/main/java/net/tafri/trackmylocation/ProfileActivity.java
+++ app/src/main/java/net/tafri/trackmylocation/ProfileActivity.java
@@ -1,14 +1,35 @@
 package net.tafri.trackmylocation;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    TextView profileFullName, profileEmail;
+    Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout = findViewById(R.id.logout);
+        profileFullName = findViewById(R.id.profileFullName);
+        profileEmail = findViewById(R.id.profileEmail);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
