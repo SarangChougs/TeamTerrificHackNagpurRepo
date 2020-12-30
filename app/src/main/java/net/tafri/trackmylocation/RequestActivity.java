98b7b340ac17283673d2ff6637f5b396fd6a677b
Aochocinco <achalurankar@gmail.com>
2020-12-30 14:19:19 +0530

    map added for request activity

--- app/src/main/java/net/tafri/trackmylocation/RequestActivity.java
+++ app/src/main/java/net/tafri/trackmylocation/RequestActivity.java
@@ -3,10 +3,13 @@ package net.tafri.trackmylocation;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,8 +20,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -26,14 +34,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
-public class RequestActivity extends AppCompatActivity {
+public class RequestActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     LocationManager locationManager;
     private static final int REQUEST_LOCATION_PERMISSION = 1;
-    Marker marker, dummyMarker;
+    Marker marker;
     LocationListener locationListener;
     int flag = 1;
     Button requestBtn;
@@ -43,8 +53,10 @@ public class RequestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
-
         requestBtn = findViewById(R.id.request);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         requestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,8 +67,67 @@ public class RequestActivity extends AppCompatActivity {
                 }
             }
         });
+        if (ActivityCompat.checkSelfPermission(RequestActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(RequestActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 3);
+        }
     }
 
+    public void setCurrentLocation(){
+        if (ActivityCompat.checkSelfPermission(RequestActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    //get the location name from latitude and longitude
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addresses =
+                                geocoder.getFromLocation(latitude, longitude, 1);
+                        String result = addresses.get(0).getLocality() + ":";
+                        result += addresses.get(0).getCountryName();
+                        LatLng latLng = new LatLng(latitude, longitude);
+                        if (marker != null) {
+                            marker.remove();
+                            marker = mMap.addMarker(new MarkerOptions().position(latLng).title(result));
+                            mMap.setMaxZoomPreference(20);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
+                        } else {
+                            marker = mMap.addMarker(new MarkerOptions().position(latLng).title(result));
+                            mMap.setMaxZoomPreference(20);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 21.0f));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } else {
+            ActivityCompat.requestPermissions(RequestActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    3);
+        }
+    }
     public void setRequest() {
         Request request = new Request("" + GlobalClass.user.getName(),
                 "" + GlobalClass.user.getUid(),
@@ -134,8 +205,16 @@ public class RequestActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        setCurrentLocation();
+    }
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        updateLocation();
+        if(requestCode == REQUEST_LOCATION_PERMISSION)
+            updateLocation();
+        if(requestCode == 3)
+            setCurrentLocation();
     }
 
     public void attachListeners() {
